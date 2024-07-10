a15caca6443bb5b2a03be5d8f86839a631da2464
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-07-10 11:04:07 +0530

    Generated: `discord-bot-project/src/features/moderation/mute.js`

---
+++ discord-bot-project/src/features/moderation/mute.js
@@ -0,0 +1,12 @@
+const muteUser = (userId, duration) => {
+    // Logic to mute a user for a specified duration
+};
+
+const unmuteUser = (userId) => {
+    // Logic to unmute a user
+};
+
+module.exports = {
+    muteUser,
+    unmuteUser
+};
